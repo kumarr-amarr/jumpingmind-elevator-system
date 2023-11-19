4783db2b940591bc2c37e881dcc086917ea82752
AMAR KUMAR <amarak2627@gmail.com>
2023-11-19 14:41:51 +0530

    Registered models in admin.py

--- elevator_system/elevator/admin.py
+++ elevator_system/elevator/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Floor,Elevator,UserRequest
 
 # Register your models here.
+@admin.register(Floor)
+class FloorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Elevator)
+class ElevetorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserRequest)
+class UserRequestAdmin(admin.ModelAdmin):
+    pass
