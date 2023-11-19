3ded89093c1837034c3b37dd3f50463fe79bdc97
AMAR KUMAR <amarak2627@gmail.com>
2023-11-19 22:15:30 +0530

    Defiend models and fields in serializers.py

--- elevator_system/elevator/serializers.py
+++ elevator_system/elevator/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from.models import Floor, Elevator, UserRequest
+
 class FloorSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Floor
+        fields = '__all__'
 
 class ElevatorSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Elevator
+        fields = '__all__'
 
 class UserRequestSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = UserRequest
+        fields = '__all__'
