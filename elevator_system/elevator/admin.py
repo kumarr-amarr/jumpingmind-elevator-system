c6d8602beaeb7dd2806a63490ff1c7fabf763a30
AMAR KUMAR <amarak2627@gmail.com>
2023-11-19 22:18:14 +0530

    changes in admin.py

--- elevator_system/elevator/admin.py
+++ elevator_system/elevator/admin.py
@@ -4,12 +4,14 @@ from .models import Floor,Elevator,UserRequest
 # Register your models here.
 @admin.register(Floor)
 class FloorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('floor_number', )
 
 @admin.register(Elevator)
 class ElevetorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('elevator_id', 'current_floor', 'direction', 'door_status', 'running', 'operational')
+    list_filter = ('direction', 'door_status', 'running', 'operational')
 
 @admin.register(UserRequest)
 class UserRequestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('elevator', 'floor_number', 'direction')
+    list_filter = ('direction', )
