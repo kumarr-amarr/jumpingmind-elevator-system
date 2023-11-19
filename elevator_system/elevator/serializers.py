02132f650c1110bc368d44d381de253ba001b085
AMAR KUMAR <amarak2627@gmail.com>
2023-11-19 14:22:29 +0530

    created serializers.py and implemented serializer classes according to model classes

---
+++ elevator_system/elevator/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+class FloorSerializer(serializers.ModelSerializer):
+    pass
+
+class ElevatorSerializer(serializers.ModelSerializer):
+    pass
+
+class UserRequestSerializer(serializers.ModelSerializer):
+    pass
