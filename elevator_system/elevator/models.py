b63d38f4596df553643be07893be77b50a9437ce
AMAR KUMAR <amarak2627@gmail.com>
2023-11-19 22:11:01 +0530

    Defiend fields in model classes

--- elevator_system/elevator/models.py
+++ elevator_system/elevator/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 class Floor(models.Model):
-    pass
-
+    floor_number = models.integerField()
 
 class Elevator(models.Model):
-    pass
+    elevator_id = models.integerField(primary_key=True)
+    current_floor = models.integerField()
+    direction = models.charField(max_length=4, default="stop")
+    door_status = models.CharField(max_length=6, default="closed")
+    running = models.BooleanField(default=False)
+    operational = models.BooleanField(default=True)
 
-
-class UserRequest(models.model):
-    pass
-
-
+class UserRequest(models.Model):
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE)
+    floor_number = models.IntegerField()
+    direction = models.CharField(max_length=4)
 
 
