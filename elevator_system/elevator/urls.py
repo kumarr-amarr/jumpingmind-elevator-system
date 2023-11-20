bdbce396b369a6a055ee3a36ce1d36b74af91456
AMAR KUMAR <amarak2627@gmail.com>
2023-11-20 13:20:17 +0530

    defiend open_door API in views.py to open door of a specific elevator

--- elevator_system/elevator/urls.py
+++ elevator_system/elevator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import initialize_elevator_system
+from .views import initialize_elevator_system, open_door
 
 urlpatterns = [
    path('initialize_elevator_system/', initialize_elevator_system, name='initialize_elevator_system'),
+   path('open_door/', open_door, name='open_door'),
 ]
--- elevator_system/elevator/views.py
+++ elevator_system/elevator/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from.models import Elevator
 
 @api_view(['POST'])
@@ -17,3 +18,11 @@ def initialize_elevator_system(request):
         Elevator.objects.create(elevator_id=i, current_floor=1)
 
     return Response({'message': f'{num_elevators} elevators initialized successfully'}, status=status.HTTP_201_CREATED)
+
+@api_view(["POST"])
+def open_door(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    elevator.door_status = 'open'
+    elevator.save()
+    return Response({'message': 'Elevator door opened'}, status=status.HTTP_200_OK)
