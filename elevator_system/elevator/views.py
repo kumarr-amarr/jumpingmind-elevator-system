17817d7ac8de665280f2e05975da6f04395f463c
AMAR KUMAR <amarak2627@gmail.com>
2023-11-20 16:55:48 +0530

    reordered apis and its urls

--- elevator_system/elevator/views.py
+++ elevator_system/elevator/views.py
@@ -20,6 +20,50 @@ def initialize_elevator_system(request):
 
     return Response({'message': f'{num_elevators} elevators initialized successfully'}, status=status.HTTP_201_CREATED)
 
+@api_view(['GET'])
+def fetch_requests_for_elevator(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    requests = UserRequest.objects.filter(elevator=elevator)
+    serializer = UserRequestSerializer(requests, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def fetch_next_destination(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    next_destination = elevator.userrequest_set.first()
+    
+    if next_destination is not None:
+        return Response({'next_destination': next_destination.floor_number})
+    else:
+        return Response({'message': 'No pending requests'}, status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['GET'])
+def fetch_elevator_direction(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    return Response({'direction': elevator.direction})
+
+@api_view(['POST'])
+def save_user_request(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    serializer = UserRequestSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save(elevator=elevator)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update_elevator_status(request):
+    elevator_id = request.data.get('elevator_id')
+    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
+    elevator.running = False
+    elevator.save()
+    return Response({'message': 'Elevator not working'}, status=status.HTTP_200_OK)
+
 @api_view(["POST"])
 def open_door(request):
     elevator_id = request.data.get('elevator_id')
@@ -54,23 +98,6 @@ def move_elevator_down(request):
     elevator.save()
     return Response({'message': 'Elevator moving down'}, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
-def fetch_next_destination(request):
-    elevator_id = request.data.get('elevator_id')
-    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
-    next_destination = elevator.userrequest_set.first()
-    
-    if next_destination is not None:
-        return Response({'next_destination': next_destination.floor_number})
-    else:
-        return Response({'message': 'No pending requests'}, status=status.HTTP_204_NO_CONTENT)
-    
-@api_view(['GET'])
-def fetch_elevator_direction(request):
-    elevator_id = request.data.get('elevator_id')
-    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
-    return Response({'direction': elevator.direction})
-
 @api_view(['POST'])
 def stop_elevator(request):
     elevator_id = request.data.get('elevator_id')
@@ -78,33 +105,6 @@ def stop_elevator(request):
     elevator.direction = 'stop'
     elevator.save()
     return Response({'message': 'Elevator stopped'}, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def save_user_request(request):
-    elevator_id = request.data.get('elevator_id')
-    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
-    serializer = UserRequestSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save(elevator=elevator)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def fetch_requests_for_elevator(request):
-    elevator_id = request.data.get('elevator_id')
-    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
-    requests = UserRequest.objects.filter(elevator=elevator)
-    serializer = UserRequestSerializer(requests, many=True)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def update_elevator_status(request):
-    elevator_id = request.data.get('elevator_id')
-    elevator = get_object_or_404(Elevator, elevator_id=elevator_id)
-    elevator.running = False
-    elevator.save()
-    return Response({'message': 'Elevator not working'}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def mark_elevator_operational(request):
